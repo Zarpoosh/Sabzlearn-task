484a50bd998591eb6cf2772dd76cd9df568140b1
Minoo zarpoosh <mzar67839@gmail.com>
2023-02-22 19:20:16 +0330

    first all commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('.nav-button').click(function(){
+        // toggleclass komak be hazf o ezafe kardan class mikone
+        $('.nav-button').toggleClass('change')
+    })
+})
